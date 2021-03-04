7b9d936ebd1f259f482113b879994a9d9fc51926
Lo-GR <diamondintheroth@gmail.com>
2021-03-03 16:28:30 -0800

    finish prototype of project
    Co-authored-by: Levi Kohler <kohler.la01@gmail.com>
    Co-authored-by: Jeroen van Seeters <vanseetersjeroen@gmail.com>

--- Tamagotchis/Controllers/TamagotchisController.cs
+++ Tamagotchis/Controllers/TamagotchisController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using Tamagotchis.Models;
 
 
@@ -6,6 +7,56 @@ namespace Tamagotchis.Controllers
 {
   public class TamagotchisController : Controller
   {
+    [HttpGet("/tamagotchis")]
+    public ActionResult Index()
+    {
+      List<Tamagotchi> allTamagotchis = Tamagotchi.GetAll();
+      return View(allTamagotchis);
+    }
+    [HttpGet("/tamagotchis/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
 
+    [HttpPost("/tamagotchis")]
+    public ActionResult Create(string name)
+    {
+      Tamagotchi newTamagotchi = new Tamagotchi(name);
+      return RedirectToAction("Index");
+    }
+    [HttpGet("/tamagotchis/{id}")]
+    public ActionResult Show(int id)
+    {
+      Tamagotchi foundTam = Tamagotchi.Find(id);
+      return View(foundTam);
+    }
+    [HttpPost("/tamagotchis/passtime")]
+    public ActionResult Update()
+    {
+      Tamagotchi.PassTime();
+      return RedirectToAction("Index");
+    }
+    [HttpPost("/tamagotchis/foodgive/{id}")]
+    public ActionResult UpdateFood(int id)
+    {
+      Tamagotchi hungryTam = Tamagotchi.Find(id);
+      hungryTam.GiveFood();
+      return RedirectToAction("Index");
+    }
+        [HttpPost("/tamagotchis/sleepnow/{id}")]
+    public ActionResult UpdateSleep(int id)
+    {
+      Tamagotchi sleepyTam = Tamagotchi.Find(id);
+      sleepyTam.SleepNow();
+      return RedirectToAction("Index");
+    }
+    [HttpPost("/tamagotchis/attentionprovide/{id}")]
+    public ActionResult UpdateAttention(int id)
+    {
+      Tamagotchi needyTam = Tamagotchi.Find(id);
+      needyTam.AttentionProvide();
+      return RedirectToAction("Index");
+    }
   }
 }
--- Tamagotchis/Models/Tamagotchis.cs
+++ Tamagotchis/Models/Tamagotchis.cs
@@ -5,6 +5,82 @@ namespace Tamagotchis.Models
 
   public class Tamagotchi
   {
+    public string Name { get; set; }
+    public int Food { get; set; }
+    public int Attention { get; set; }
+    public int Rest { get; set; }
+    public int Id { get; }
+    public bool Alive { get; set; }
+    
+    private static List<Tamagotchi> _instances = new List<Tamagotchi> {};
 
+    public Tamagotchi(string name)
+    {
+      Name = name;
+      Food = 100;
+      Attention = 100;
+      Rest = 100;
+      Alive = true;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+    //PassTime
+    public static void PassTime()
+    {
+      foreach (Tamagotchi element in _instances)
+      {
+        element.Food = element.Food - 5;
+        element.Rest = element.Rest - 5;
+        element.Attention = element.Attention - 5;
+        if (element.Food <= 0)
+        {
+          element.Alive = false;
+        }
+
+      }
+    }
+    public void GiveFood()
+    {
+      Food = Food + 20;
+      Rest = Rest - 15;
+      PassTime();
+    }
+      public void AttentionProvide()
+    {
+      Attention = Attention + 20;
+      Food = Food - 15;
+      PassTime();
+    }
+      public void SleepNow()
+    {
+      Rest = 100;
+      Food = Food - 50;
+      Attention = Attention -50;
+      PassTime();
+    }
+
+    public static List<Tamagotchi> GetAll()
+    {
+      return _instances;
+    }
+    public static Tamagotchi Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+    public string AliveOrDead()
+    {
+      if (Alive)
+      {
+        return "Alive";
+      }
+      else
+      {
+        return "ded";
+      }
+    }
+    //     public static Item Find(int searchId)
+    // {
+    //   return _instances[searchId-1];
+    // }
   }
 }
